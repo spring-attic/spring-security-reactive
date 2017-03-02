c74d53670e15f4579e609f281de5d9755ed9d274
Rob Winch <rwinch@gopivotal.com>
2017-03-02 13:51:53 -0600

    Use Spring 5.0.0.M5

--- sample/src/test/java/sample/SecurityTests.java
+++ sample/src/test/java/sample/SecurityTests.java
@@ -15,11 +15,9 @@
  */
 package sample;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 import java.nio.charset.Charset;
-import java.time.Duration;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
@@ -29,8 +27,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.ResolvableType;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.ExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -39,7 +35,6 @@ import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SecurityTests {
-	private static final ResolvableType MAP_OF_STRING_STRING = ResolvableType.forClassWithGenerics(Map.class, String.class, String.class);
 
 	private WebTestClient rest;
 
@@ -55,106 +50,90 @@ public class SecurityTests {
 
 	@Test
 	public void basicRequired() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.get()
 			.uri("/users")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+			.expectStatus().isUnauthorized();
 	}
 
 	@Test
 	public void basicWorks() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.filter(robsCredentials())
 			.get()
 			.uri("/users")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.OK);
+			.expectStatus().isOk();
 	}
 
 	@Test
 	public void authorizationAdmin401() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.filter(robsCredentials())
 			.get()
 			.uri("/admin")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+			.expectStatus().isUnauthorized();
 	}
 
 	@Test
 	public void authorizationAdmin200() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.filter(adminCredentials())
 			.get()
 			.uri("/admin")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.OK);
+			.expectStatus().isOk();
 	}
 
 	@Test
 	public void basicMissingUser401() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.filter(basicAuthentication("missing-user", "password"))
 			.get()
 			.uri("/admin")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+			.expectStatus().isUnauthorized();
 	}
 
 	@Test
 	public void basicInvalidPassword401() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.filter(basicAuthentication("rob", "invalid"))
 			.get()
 			.uri("/admin")
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+			.expectStatus().isUnauthorized();
 	}
 
 	@Test
 	public void basicInvalidParts401() throws Exception {
-		ExchangeResult<String> result = this.rest
+		this.rest
 			.get()
 			.uri("/admin")
 			.header("Authorization", "Basic " + base64Encode("no colon"))
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+			.expectStatus().isUnauthorized();
 	}
 
 	@Test
 	public void sessionWorks() throws Exception {
-		ExchangeResult<String> result = this.rest
+		ExchangeResult result = this.rest
 				.filter(robsCredentials())
 				.get()
 				.uri("/users")
 				.exchange()
-				.decodeEntity(String.class);
+				.returnResult();
 
 		String session = result.getResponseHeaders().getFirst("Set-Cookie");
 
-		result = this.rest
+		this.rest
 			.get()
 			.uri("/users")
 			.header("Cookie", session)
 			.exchange()
-			.decodeEntity(String.class);
-
-		assertThat(result.getResponseStatus()).isEqualTo(HttpStatus.OK);
+			.expectStatus().isOk();
 	}
 
 	@Test
@@ -166,10 +145,8 @@ public class SecurityTests {
 			.get()
 			.uri("/me")
 			.exchange()
-			.decodeEntity(MAP_OF_STRING_STRING)
-			.assertThat()
-			.status().isOk()
-			.bodyEquals(expected);
+			.expectStatus().isOk()
+			.expectBody().map(String.class, String.class).isEqualTo(expected);
 	}
 
 	@Test
@@ -181,10 +158,8 @@ public class SecurityTests {
 				.get()
 				.uri("/principal")
 				.exchange()
-				.decodeEntity(MAP_OF_STRING_STRING)
-				.assertThat()
-				.status().isOk()
-				.bodyEquals(expected);
+				.expectStatus().isOk()
+				.expectBody().map(String.class, String.class).isEqualTo(expected);
 	}
 
 	private ExchangeFilterFunction robsCredentials() {
